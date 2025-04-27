945e5066dcfaed9ed5e7ccbc178f4e797aaac17b
Navin-VijayaKumar <navinv10122004@gmail.com>
2025-04-27 18:50:06 +0530

    Routing

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,8 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center items-center bg-white">
+    <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-5xl text-accent font-bold">Welcome</Text>
+
     </View>
+
   );
 }
